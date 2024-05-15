469e4ddf34c547e21134ffa50fd0e326d795e547
Movash <andrushhenko97@gmail.com>
2024-05-16 00:55:45 +0300

    remove_or_rename

--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { InfinitySpin } from 'react-loader-spinner';
-
-const Loader = () => (
-  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <InfinitySpin />
-  </div>
-);
-
-export default Loader;
