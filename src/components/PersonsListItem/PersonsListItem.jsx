c3f2276934db1b8facb5872cfdb7f9f9d5878f9c
Movash <andrushhenko97@gmail.com>
2024-05-15 21:37:12 +0300

    create_persons_list_item

---
+++ src/components/PersonsListItem/PersonsListItem.jsx
@@ -0,0 +1,45 @@
+import ModalPersons from 'components/ModalPersons/ModalPersons';
+import React, { useState } from 'react';
+
+const PersonsListItem = ({ person }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { name, status, image, species } = person;
+
+  const openModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <>
+      <li
+        className="relative flex flex-col justify-between w-[266px] h-[426px] rounded-[14px] bg-[#ffe0b6] shadow-xl"
+        onClick={openModalToggle}
+      >
+        <div className="flex flex-col justify-between items-center h-full">
+          <img
+            className="h-[300px] object-cover rounded-tl-[14px] rounded-tr-[14px]"
+            src={image}
+            alt="person"
+          />
+          <div className="flex flex-col justify-center items-center h-[126px] gap-2">
+            <h2 className="font-bold text-xl text-[#121417] text-center">
+              {name}
+            </h2>
+            <h2 className="font-medium text-lg text-[#121417] text-center">
+              {status} - {species}
+            </h2>
+          </div>
+        </div>
+      </li>
+      <ModalPersons
+        key={name}
+        person={person}
+        isOpen={isModalOpen}
+        onClose={openModalToggle}
+      />
+    </>
+  );
+};
+
+export default PersonsListItem;
