1f11c28e4ea6fbd028b281d12ec0e3e2610742ff
Movash <andrushhenko97@gmail.com>
2024-05-18 20:48:03 +0300

    create_response_interface

--- src/api/api.types.ts
+++ src/api/api.types.ts
@@ -1,3 +1,15 @@
+export interface Response {
+  info: Info;
+  results: Character[];
+}
+
+export interface Info {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
 export interface Character {
   id: number;
   name: string;
