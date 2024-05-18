e166c0be841b5391b1fc06cb6775134a8757f4d4
Movash <andrushhenko97@gmail.com>
2024-05-18 20:49:14 +0300

    update_pagination

--- src/components/CustomPagination/CustomPagination.tsx
+++ src/components/CustomPagination/CustomPagination.tsx
@@ -1,33 +1,65 @@
 import { FC } from 'react';
+import { PaginationInfo } from '../../App';
 
 interface PaginationProps {
-  personsPerPage: number;
-  totalPersons: number;
+  paginationInfo: PaginationInfo;
   paginate: (pageNumber: number) => void;
 }
 
 const CustomPagination: FC<PaginationProps> = ({
-  personsPerPage,
-  totalPersons,
+  paginationInfo,
   paginate,
 }) => {
+  const { pages, next, prev } = paginationInfo;
+
   const pageNumbers: number[] = [];
 
-  for (let i = 1; i <= Math.ceil(totalPersons / personsPerPage); i++) {
+  for (let i = 1; i <= pages; i++) {
     pageNumbers.push(i);
   }
 
+  const handleNextPage = async () => {
+    if (next) {
+      const nextPageNumber = parsePageNumber(next);
+      paginate(nextPageNumber);
+    }
+  };
+
+  const handlePrevPage = async () => {
+    if (prev) {
+      const prevPageNumber = parsePageNumber(prev);
+      paginate(prevPageNumber);
+    }
+  };
+
+  const parsePageNumber = (url: string): number => {
+    const parsedUrl = new URL(url);
+    const page = parsedUrl.searchParams.get('page');
+    return page ? parseInt(page, 10) : 1;
+  };
+
   return (
-    <div>
-      <ul>
+    <div className="flex justify-center">
+      <button onClick={handlePrevPage} disabled={!prev}>
+        Previous
+      </button>
+      <ul className="flex">
         {pageNumbers.map(number => (
           <li key={number}>
-            <a href="!#" onClick={() => paginate(number)}>
+            <button
+              onClick={e => {
+                e.preventDefault();
+                paginate(number);
+              }}
+            >
               {number}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
+      <button onClick={handleNextPage} disabled={!next}>
+        Next
+      </button>
     </div>
   );
 };
