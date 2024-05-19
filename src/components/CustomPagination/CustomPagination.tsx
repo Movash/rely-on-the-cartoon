a06ebc2fad905b5c61b069f25a005bbb962cc187
Movash <andrushhenko97@gmail.com>
2024-05-19 15:31:29 +0300

    add_styles

--- src/components/CustomPagination/CustomPagination.tsx
+++ src/components/CustomPagination/CustomPagination.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
 import { PaginationInfo } from '../../App';
+import sprite from '../../assets/icons/sprite.svg';
 
 interface PaginationProps {
   paginationInfo: PaginationInfo;
   paginate: (pageNumber: number) => void;
+  currentPage: number;
 }
 
 const CustomPagination: FC<PaginationProps> = ({
   paginationInfo,
   paginate,
+  currentPage,
 }) => {
   const { pages, next, prev } = paginationInfo;
 
@@ -39,14 +42,27 @@ const CustomPagination: FC<PaginationProps> = ({
   };
 
   return (
-    <div className="flex justify-center">
-      <button onClick={handlePrevPage} disabled={!prev}>
-        Previous
+    <div className="flex justify-center gap-[10px]">
+      <button
+        className={`bg-slate-300 rounded-full w-7 h-7 flex items-center justify-center transition-colors ${
+          prev ? 'hover:bg-slate-400 active:bg-slate-500' : 'invisible'
+        }`}
+        onClick={handlePrevPage}
+        disabled={!prev}
+      >
+        <svg className="w-[24px] h-[24px] stroke-[#121417]">
+          <use href={`${sprite}#icon-arrow-left`} />
+        </svg>
       </button>
-      <ul className="flex">
+      <ul className="flex gap-[2px]">
         {pageNumbers.map(number => (
           <li key={number}>
             <button
+              className={`rounded-full w-7 h-7 pb-[1px] transition-colors ${
+                number === currentPage
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-slate-300 hover:bg-slate-400 active:bg-slate-500'
+              }`}
               onClick={e => {
                 e.preventDefault();
                 paginate(number);
@@ -57,8 +73,16 @@ const CustomPagination: FC<PaginationProps> = ({
           </li>
         ))}
       </ul>
-      <button onClick={handleNextPage} disabled={!next}>
-        Next
+      <button
+        className={`bg-slate-300 rounded-full w-7 h-7 flex items-center justify-center transition-colors ${
+          next ? 'hover:bg-slate-400 active:bg-slate-500' : 'invisible'
+        }`}
+        onClick={handleNextPage}
+        disabled={!next}
+      >
+        <svg className="w-[24px] h-[24px] stroke-[#121417]">
+          <use href={`${sprite}#icon-arrow-right`} />
+        </svg>
       </button>
     </div>
   );
