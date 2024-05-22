1b1603c3a0aeb813388b5f0c4a73c767d5d9a653
Movash <andrushhenko97@gmail.com>
2024-05-22 17:25:21 +0300

    add_react_query

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,10 @@ import { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import CustomPagination from './components/CustomPagination/CustomPagination';
 import { useSearchParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 export const App = () => {
-  const [persons, setPersons] = useState<Character[]>([]);
   const [filteredPersons, setFilteredPersons] = useState<Character[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -25,90 +24,71 @@ export const App = () => {
   const currentPage = pageParam ? parseInt(pageParam, 10) : 1;
   const personsPerPage = 20;
 
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        setIsLoading(true);
-        const allCharacters: Character[] = [];
-        let page = 1;
-        let hasMore = true;
+  const { data, isLoading, isError } = useQuery(
+    ['characters', currentPage],
+    async () => {
+      const allCharacters: Character[] = [];
+      let page = 1;
+      let hasMore = true;
 
-        while (hasMore) {
-          const response = await getCharacters({ page });
-          const { info, results } = response.data;
-          allCharacters.push(...results);
-          hasMore = !!info.next;
-          page += 1;
+      while (hasMore) {
+        const response = await getCharacters({ page });
+        const { info, results } = response.data;
+        allCharacters.push(...results);
+        hasMore = !!info.next;
+        page += 1;
+      }
+
+      return allCharacters;
+    },
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  useEffect(() => {
+    if (data) {
+      let filtered = [...data];
+
+      if (nameParam) {
+        filtered = filtered.filter(person =>
+          person.name.toLowerCase().includes(nameParam.toLowerCase())
+        );
+      }
+
+      if (speciesParam) {
+        filtered = filtered.filter(person =>
+          person.species.toLowerCase().includes(speciesParam.toLowerCase())
+        );
+      }
+
+      if (statusParam) {
+        filtered = filtered.filter(
+          person => person.status.toLowerCase() === statusParam.toLowerCase()
+        );
+      }
+
+      if (genderParam) {
+        filtered = filtered.filter(
+          person => person.gender.toLowerCase() === genderParam.toLowerCase()
+        );
+      }
+
+      if (sortOrderParam) {
+        if (sortOrderParam === 'ascending') {
+          filtered.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortOrderParam === 'descending') {
+          filtered.sort((a, b) => b.name.localeCompare(a.name));
         }
-
-        setPersons(allCharacters);
-        setFilteredPersons(allCharacters);
-      } catch (error) {
-        toast.error('Something went wrong.');
-      } finally {
-        setIsLoading(false);
       }
-    };
-    fetchCharacters();
-  }, []);
 
-  useEffect(() => {
-    let filtered = [...persons];
-
-    if (nameParam) {
-      filtered = filtered.filter(person =>
-        person.name.toLowerCase().includes(nameParam.toLowerCase())
-      );
+      setFilteredPersons(filtered);
     }
-
-    if (speciesParam) {
-      filtered = filtered.filter(person =>
-        person.species.toLowerCase().includes(speciesParam.toLowerCase())
-      );
-    }
-
-    if (statusParam) {
-      filtered = filtered.filter(
-        person => person.status.toLowerCase() === statusParam.toLowerCase()
-      );
-    }
-
-    if (genderParam) {
-      filtered = filtered.filter(
-        person => person.gender.toLowerCase() === genderParam.toLowerCase()
-      );
-    }
-
-    if (sortOrderParam) {
-      if (sortOrderParam === 'ascending') {
-        filtered.sort((a, b) => a.name.localeCompare(b.name));
-      } else if (sortOrderParam === 'descending') {
-        filtered.sort((a, b) => b.name.localeCompare(a.name));
-      }
-    }
-
-    setFilteredPersons(filtered);
-  }, [
-    nameParam,
-    speciesParam,
-    statusParam,
-    genderParam,
-    sortOrderParam,
-    persons,
-  ]);
-
-  const sortPersons = (personsToSort: Character[], sortOrder: string) => {
-    if (sortOrder === 'ascending') {
-      return personsToSort.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortOrder === 'descending') {
-      return personsToSort.sort((a, b) => b.name.localeCompare(a.name));
-    }
-    return personsToSort;
-  };
+  }, [nameParam, speciesParam, statusParam, genderParam, sortOrderParam, data]);
 
   const indexOfLastPerson = currentPage * personsPerPage;
   const indexOfFirstPerson = indexOfLastPerson - personsPerPage;
-  const currentPersons = sortPersons(filteredPersons, sortOrderParam).slice(
+  const currentPersons = filteredPersons.slice(
     indexOfFirstPerson,
     indexOfLastPerson
   );
@@ -130,40 +110,42 @@ export const App = () => {
     setSearchParams(params);
   };
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    toast.error('Something went wrong.');
+    return <div>Something went wrong</div>;
+  }
+
   return (
     <div className="w-[1440px] mx-auto my-[50px] px-[15px]">
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Filter
-            onFilterChange={handleFilterChange}
-            initialFilters={{
-              name: nameParam,
-              species: speciesParam,
-              status: statusParam,
-              gender: genderParam,
-              sortOrder: sortOrderParam,
-            }}
-          />
-          <PersonsList persons={currentPersons} />
-          {filteredPersons.length > personsPerPage && (
-            <CustomPagination
-              paginationInfo={{
-                count: filteredPersons.length,
-                pages: Math.ceil(filteredPersons.length / personsPerPage),
-                next:
-                  currentPage <
-                  Math.ceil(filteredPersons.length / personsPerPage)
-                    ? (currentPage + 1).toString()
-                    : null,
-                prev: currentPage > 1 ? (currentPage - 1).toString() : null,
-              }}
-              paginate={paginate}
-              currentPage={currentPage}
-            />
-          )}
-        </>
+      <Filter
+        onFilterChange={handleFilterChange}
+        initialFilters={{
+          name: nameParam,
+          species: speciesParam,
+          status: statusParam,
+          gender: genderParam,
+          sortOrder: sortOrderParam,
+        }}
+      />
+      <PersonsList persons={currentPersons} />
+      {filteredPersons.length > personsPerPage && (
+        <CustomPagination
+          paginationInfo={{
+            count: filteredPersons.length,
+            pages: Math.ceil(filteredPersons.length / personsPerPage),
+            next:
+              currentPage < Math.ceil(filteredPersons.length / personsPerPage)
+                ? (currentPage + 1).toString()
+                : null,
+            prev: currentPage > 1 ? (currentPage - 1).toString() : null,
+          }}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       )}
       <Toaster />
     </div>
