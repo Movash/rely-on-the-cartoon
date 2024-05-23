0b818963e4b7399b9f659811ebd2de2e859858ff
Movash <andrushhenko97@gmail.com>
2024-05-24 01:18:53 +0300

    add_html_element

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root')!;
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(
   <React.StrictMode>
